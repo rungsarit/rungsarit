f5296d8a1ff9ebb0b6e3e3007a9cf5b0e0494058
rungsarit <42631147+rungsarit@users.noreply.github.com>
2018-09-14 12:39:35 +0700

    Update connect-db.php

--- db/connect-db.php
+++ db/connect-db.php
@@ -1,8 +1,8 @@
 <?php
   $server = "us-cdbr-iron-east-01.cleardb.net";
-    $username = "bde5ae12f51085";
-    $password = "3c146dba";
-    $db = "heroku_4535866affeb67f";
+    $username = "bde42c9156a905";
+    $password = "0ba833ce";
+    $db = "heroku_ad94462b3f7f2f2";
     $conn = new mysqli($server, $username, $password, $db);
     // Check connection
     if ($conn->connect_error) {
